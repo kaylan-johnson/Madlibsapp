a4e723046d3c7344f338df9370133f30d3009787
kaylan-johnson <drkaylan@gmail.com>
2019-06-20 14:14:00 -0600

    added clear button

--- src/components/madlib_form.js
+++ src/components/madlib_form.js
@@ -58,6 +58,27 @@ class MadlibForm extends Component {
         event.preventDefault();
     }.bind(this);
 
+    handleClick = function() {
+        this.setState({
+            completedForm: false,
+            color: '',
+            pluralNoun: '',
+            adjectiveOne: '',
+            celebrityOne: '',
+            adjectiveTwo: '',
+            nounOne: '',
+            numberOne: '',
+            numberTwo: '',
+        });
+    }.bind(this)
+
+    renderButton = function() {
+        if(this.state.completedForm) {
+            return <a className="clear-button" onClick={this.handleClick}>Clear Mad Lib</a>
+        }
+        return <input type="submit" className="generate-button" value="Generate Mad Lib" /> 
+    }
+
 
     render() {
 
@@ -87,7 +108,7 @@ class MadlibForm extends Component {
                     </Row>
                     <Row>
                         <Col md="12" className="button-wrapper">
-                            <input type="submit" className="generate-button" value="Generate Mad Lib" />
+                        {this.renderButton()}
                         </Col>
                     </Row>
                 </form>
